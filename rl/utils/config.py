4271f2e14af9c28bf1476c7687915de56e0c8732
huangzhe <huangzhe@sensetime.com>
2019-02-02 01:44:33 +0800

    add cfg parser

--- rl/utils/config.py
+++ rl/utils/config.py
@@ -1,13 +1,31 @@
-import argpase
+import argparse
 import yaml
 
 
-def parse_config():
-    pass
+class DotConfig(dict):
+    
+    def __getattr__(self, key):
+        try:
+            value = self[key]
+        except KeyError:
+            return super().__getattr__(key)
+        if isinstance(value, dict):
+            return DotConfig(value)
+        return value
 
-
-class DotConfig:
-    # TODO: create dot-accessible config
-    def __init__(self):
-        pass
-
+    
+def get_config(filename=None):
+    if filename is None:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--config', '-cfg', type=str, default='config.yaml')
+        _cfg_name = parser.parse_args()
+    else:
+        _cfg_name = filename
+    
+    assert type(_cfg_name) == str, 'invalid config filename specified (not a string type)'
+    assert _cfg_name.rsplit('.', 1)[-1] == 'yaml', 'unspported config file type yet (not .yaml)'
+    
+    with open(_cfg_name, 'r') as _cfg_f:
+        _raw_cfg = yaml.load(_cfg_f)
+    
+    return DotConfig(_raw_cfg)
