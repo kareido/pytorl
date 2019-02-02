9677ae76b1fb621699d05ddc893893815bd5e15e
zhe <zackhwang@qq.com>
2019-02-03 03:05:21 +0800

    add memory module

---
+++ rl/utils/memory.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+
+class _Single_Process_Replay:
+    def __init__(self):
+        pass
+    
+    
+class _Distributed_Replay:
+    def __init__(self):
+        pass
+
+    
+class Replay:
+    def __init__(self):
+        pass
+        
