7e441f37f15915b47c92b50945032ae83c7c1349
zhe <zackhwang@qq.com>
2019-02-11 21:02:40 +0800

    update srun

--- examples/deep_q_learning/SRUN.sh
+++ examples/deep_q_learning/SRUN.sh
@@ -1,2 +1,2 @@
 mkdir -p ./checkpoint
-srun -p SenseMediaF --gres gpu:1 python train.py 2>&1 | tee -a ./checkpoint/log.txt
+srun -p SenseMediaF --gres gpu:1 python learning.py 2>&1 | tee -a ./checkpoint/log.txt
